93cac8619e3d0ac16cab6385c9f1369239b73e30
thatbulbul <bulbulsharma3363@gmail.com>
2026-04-28 02:12:15 +0530

    awards section and hero section

--- src/landing_page/home/Awards.js
+++ src/landing_page/home/Awards.js
@@ -1,36 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react';
 function Awards() {
-    return ( 
+    return (  
         <div className='container'>
             <div className='row'>
-                <div className='col-6'>
-                    <img src='media/images/largestBroker.svg' alt ='award' className='mb-4 p-4'/>
+                <div className='col-6 mt-5'>
+                    <img src='media/images/largestBroker.svg'></img>
                 </div>
-
-                <div className='col-6'></div>
-                <h1>Largest stock broker in India</h1>
-                <p>1.5+ Crore Zerodha clients contribute to over 15% of all volumes.</p>
-                <div className='row'>
-                    <div className='col-6'>
-                         <ul>
-                    <li> <p>Future and option</p></li>
-                    <li> <p>Commodity derivatives</p></li>
-                    <li> <p>Currency derivatives</p></li>
-                </ul>
+                <div className='col-6 mt-5 p-4'>
+                    <h1>Largest Stock brocker in India</h1>
+                    <p className='mt-4 mb-4'> That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh crores of equity investments.</p>
+                    <div className='row mt-4 mb-4'>
+                        <div className='col-6 '>
+                    <ul>
+                        <li><p>profit</p></li>
+                        <li><p>future</p></li>
+                        <li><p>Good</p></li>
+                    </ul>
                     </div>
-                </div>
-                <div className='row'>
                     <div className='col-6'>
-                         <ul>
-                    <li> <p>Stock and IPOs</p></li>
-                    <li> <p>Direct Mutual Funds</p></li>
-                    <li> <p>Bonds and others</p></li>
-                </ul>
+                        <ul>
+                        <li><p>plans</p></li>
+                        <li><p>future secure</p></li>
+                        <li><p>Good investments</p></li>
+                    </ul>
+                    </div>
+                    <img src='media/images/pressLogos.png' alt='brands' style={{width:"90%"}}/>
                     </div>
                 </div>
             </div>
         </div>
-     );
+    );
 }
 
 export default Awards;
--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -1,21 +1,15 @@
-import React from 'react'
-function Hero() {
-    return (
-        <div className='container p-5'>
+import React, { Component } from 'react';
+
+function Hero(){
+    return(
+        <div className='container pt-3'>
             <div className='row text-center'>
-                <img src='media/images/homeHero.png' alt='hero' className='mb-5'/>
-                <h1 className='mt-5'>
-                    Invest in everything
-                </h1>
-                <p>
-                    Online platform to invest in stocks, derivatives, mutual funds and more
-                </p>
-                <button className='p-2 btn btn-primary fs-5' style={{width:'30%',margin:'0 auto'}}>
-                    Sign up now
-                </button>
+                <img src='media/images/homeHero.png' alt="hero-image" className='mb-2'/>
+                <h1 className='mt-5'>Invest in everything</h1>
+                <p className='mb-3'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button className='p-3 btn btn-primary'style={{width:"20%", margin:"0 auto"}}>Sign up for Free</button>
             </div>
         </div>
-      );
+    );
 }
-
 export default Hero;
