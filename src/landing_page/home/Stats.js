2d3883ed1a7a09791940a4a9d6006e640157d362
thatbulbul <bulbulsharma3363@gmail.com>
2026-04-28 03:17:42 +0530

    pricing and stats section are updated

--- src/landing_page/home/Stats.js
+++ src/landing_page/home/Stats.js
@@ -1,7 +1,32 @@
 import React from 'react'
 function Stats() {
     return ( 
-        <h1>Stats</h1>
+        <div className="container p-3">
+            <div className='row p-5'>
+                <div className='col-6 p-5'>
+                    <h1 className='fs-2 mb-3'>Trust with confidence</h1>
+                    <h2 className='fs-4'>Customer-first always</h2>
+                    <p className='text-muted'>That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh crores of equity investments, making us India’s largest broker; contributing to 15% of daily retail exchange volumes in India.</p>
+
+                    <h2 className='fs-4'>No spam or gimmicks</h2>
+                    <p className='text-muted'>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like.</p>
+
+                    <h2 className='fs-4'>The Zerodha universe</h2>
+                    <p className='text-muted'>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
+
+                    <h2 className='fs-4'>Do better with money</h2>
+                    <p className='text-muted'>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
+
+                </div>
+                <div className='col-6 '>
+                    <img src='media/images/ecosystem.png' style={{width:"90%"}}/>
+                    <div lassName='text-center'>
+                        <a href='google.com'className='mx-5' style={{textDecoration:"none"}}>explore our products</a>
+                        <a href='google.com'className='mx-5' style={{textDecoration:"none"}}>Try kit</a>
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
 
